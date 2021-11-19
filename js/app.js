e40303e595d2724e6f6fdbe6470f8edafa86b429
Jaqueline Lin <xjackieco@gmail.com>
2021-11-18 22:12:29 -0500

    add play again button

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const qwerty = document.getElementById("qwerty");
 const phrase = document.getElementById("phrase");
 const startGame = document.querySelector(".btn__reset");
 const overlay = document.getElementById("overlay");
+const ul = document.querySelector('#phrase ul');
 
 
 // track number of wrong guesses
@@ -101,10 +102,36 @@ function checkWin() {
         overlay.style.display = 'flex';
         overlay.className = 'win';
         message.textContent = 'YAAAAY! YOU WON!!!';
+        playAgain();
     } else if (missed > 4){
         overlay.style.display = 'flex';
         overlay.className = 'lose';
         message.textContent = 'Oh no! You Lost!! No worries, you can try again!';
+        playAgain();
     }
+    
+}
 
+function playAgain() {
+startGame.textContent = 'Play again';
+
+missed = 0;
+ul.textContent ='';
+const choseLetters = document.querySelectorAll('.chosen');
+
+
+for (let i = 0; i < choseLetters.length; i++){
+    choseLetters[i].classList.remove('chosen');
+    choseLetters[i].disabled = false;
+}
+
+
+const getPhrase = getRandomPhrasesAsArray(phrases);
+addPhraseToDisplay(getPhrase);
+
+const lives = document.querySelectorAll('.tries img');
+    for (let i=0; i<lives.length; i++) {
+        lives[i].src = 'images/liveHeart.png';
+    }   
+   
 }
